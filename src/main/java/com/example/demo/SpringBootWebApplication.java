41f0d189d91f3f26c29dfeb184747b68b110e5f2
Akriti Mishra <73214701+akriti6101@users.noreply.github.com>
2023-03-28 00:32:51 +0530

    commit

---
+++ src/main/java/com/example/demo/SpringBootWebApplication.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+
+import com.example.demo.controller.UserService;
+
+@SpringBootApplication
+public class SpringBootWebApplication {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(SpringBootWebApplication.class, args);
+	}
+
+}
